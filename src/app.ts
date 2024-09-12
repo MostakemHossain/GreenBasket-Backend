c6d5bbb5a85bc4b4bbffbc5763a049768d6939e6
MostakemHossain <mostakememon123@gmail.com>
2024-09-12 20:17:40 +0600

    validate request added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 
 // express
@@ -15,5 +17,11 @@ app.use("/api/v1", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Green Basket API");
 });
+
+// global error handler
+app.use(globalErrorHandler);
+
+// not Found
+app.use(notFound);
 
 export default app;
